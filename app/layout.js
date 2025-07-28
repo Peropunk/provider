abb9bfd7fccee9ae06dadb94d6412adf32f716d9
Peropunk <11rajayush@gmail.com>
2025-07-28 12:32:53 +0530

    floating buttons

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MaxWidthWrapper from "./components/MaxWidthWrapper";
 import Navbar from './components/Navbar';
+import FloatingActionButtons from './components/FloatingActionButtons';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
         <Navbar/>
        
         {children}
+        <FloatingActionButtons />
        
       </body>
     </html>
